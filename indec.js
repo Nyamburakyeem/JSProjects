d5e9c6b85df2bb4817336597e980f06f48e4a361
Rosekim <kyeemrosemary@gmail.com>
2025-06-25 06:13:41 +0300

    yes

---
+++ indec.js
@@ -0,0 +1,8 @@
+const inputbtnElement = document.querySelector(".inputbtn");
+const inputTaskElement = document.querySelector(".inputtask");
+const writeTaskElement = document.querySelector("#writetask");
+const quoteElement = document.querySelector(".quote");
+
+
+
+
